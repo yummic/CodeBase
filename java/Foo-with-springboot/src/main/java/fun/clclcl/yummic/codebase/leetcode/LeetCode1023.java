4cd689f96bef203601bdc807e406f96188e169ca
Yummi Cc <mengsanjun@163.com>
2025-02-20 19:44:47 +0800

    Update LeetCode1023.java
    Test iPad edit

--- java/Foo-with-springboot/src/main/java/fun/clclcl/yummic/codebase/leetcode/LeetCode1023.java
+++ java/Foo-with-springboot/src/main/java/fun/clclcl/yummic/codebase/leetcode/LeetCode1023.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LeetCode1023 {
-
+    // edit with ipad
     public List<Boolean> camelMatch(String[] queries, String pattern) {
 
         List<Boolean> result = new ArrayList<>();
